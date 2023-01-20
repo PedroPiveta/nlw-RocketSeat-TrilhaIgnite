27b7467b8e0d4c91eb5ad137aa6ab1afe9082816
PedroPiveta <ppivetabarrotti208@gmail.com>
2023-01-20 20:59:24 -0300

    habits interface

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -5,6 +5,7 @@ import { generateDatesFromYearBeginning } from '../utils/generate-dates-from-yea
 
 import { HabitDay, DAY_SIZE } from '../components/HabitDay';
 import { Header } from '../components/Header';
+import { useNavigation } from '@react-navigation/native'
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const datesFromYearStart = generateDatesFromYearBeginning();
@@ -12,6 +13,8 @@ const minimunSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+
+    const { navigate } = useNavigation()
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -39,6 +42,7 @@ export function Home() {
                         datesFromYearStart.map(date => (
                             <HabitDay
                                 key={date.toISOString()}
+                                onPress={() => {navigate('habit', {date: date.toISOString()})}}
                             />
                         ))
                     }
