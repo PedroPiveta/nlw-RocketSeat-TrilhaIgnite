e7b9fc1c31c4cbfd5b8dfa7b275b8e5068c1f89c
PedroPiveta <ppivetabarrotti208@gmail.com>
2023-01-20 21:33:43 -0300

    checkboxes creation

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,17 @@
+import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 
-export function NewhabitForm () {
+const availableWeekDays = [
+    'Domingo',
+    'Segunda-feira',
+    'Terça-feria',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado'
+]
+
+export function NewhabitForm() {
     return (
         <form className="w-full flex flex-col mt-6">
             <label htmlFor="title" className="font-semibold leading-tight">
@@ -19,9 +30,32 @@ export function NewhabitForm () {
                 Qual a recorrência?
             </label>
 
+            <div className="flex flex-col gap-2 mt-3">
+                {availableWeekDays.map(weekDay => {
+                    return (
+                        <Checkbox.Root
+                            key={weekDay}
+                            className='flex items-center gap-3 group'
+                        >
+                            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                                <Checkbox.Indicator>
+                                    <Check size={20} className='text-white' />
+                                </Checkbox.Indicator>
+                            </div>
+
+                            <span className='text-white leading-tight'>
+                                { weekDay }
+                            </span>
+                        </Checkbox.Root>
+                    )
+                })}
+
+
+            </div>
+
             <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
                 <Check size={20} weight="bold" />
-                Confirmar 
+                Confirmar
             </button>
         </form>
     )
